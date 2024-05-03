f6fad56dd5cb5188ed351164fdadc9a1f2ad4528
Rob Prouse <rob@prouse.org>
2024-05-02 21:52:31 -0400

    Pull in all of the opcode data

---
+++ ParseOpcodes/Opcode.cs
@@ -0,0 +1,18 @@
+namespace ParseOpcodes;
+
+// Open and read in "6502 Instructions.txt"
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+public class Opcode
+{
+    public byte Byte { get; set; }
+    public string Mnemonic { get; set; }
+    public string Operand { get; set; }
+    public byte Bytes { get; set; }
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return $"0x{Byte:X2} {Mnemonic} {Operand} {Bytes} {Description}";
+    }
+}   
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
--- ParseOpcodes/Program.cs
+++ ParseOpcodes/Program.cs
@@ -1,39 +1,39 @@
-// Open and read in "6502 Instructions.txt"
 using System.Globalization;
+using ParseOpcodes;
 
+// Read in the descriptions of the 6502 opcodes
+var descriptions = File.ReadAllLines("Files/6502 Instruction Set Table.txt")
+    .Select(line => line.Split(' ', 2))
+    .ToDictionary(parts => parts[0], parts => parts[1]);
+
+// Read in the length of the opcodes
+var lengths = File.ReadAllLines("Files/6502 Address Modes.txt")
+    .Select(line => line.Split(' '))
+    .ToDictionary(parts => parts[0], parts => byte.Parse(parts[1]));
+
+// Open and read in "6502 Instructions.txt"
 var instructions = File.ReadAllLines("Files/6502 Instructions.txt")
     .Select(line => line.Split(' '))
     .Select(parts =>
     {
-        byte b = (byte)Byte.Parse(parts[0], NumberStyles.HexNumber);
+        byte b = byte.Parse(parts[0], NumberStyles.HexNumber);
         string operand = string.Empty;
+        byte bytes = 0;
         if (parts.Length == 3)
         {
             operand = parts[2];
+            bytes = lengths[parts[2]];
         }
         return new Opcode
         {
             Byte = b,
             Mnemonic = parts[1],
-            Operand = operand
+            Operand = operand,
+            Description = descriptions[parts[1].Substring(0, 3)],
+            Bytes = bytes
         };
     })
     .ToList();
 
 instructions
     .ForEach(opcode => Console.WriteLine(opcode));
-
-
-public class Opcode
-{
-    public byte Byte { get; set; }
-    public string Mnemonic { get; set; }
-    public string Operand { get; set; }
-    public byte Bytes { get; set; }
-    public string Description { get; set; }
-
-    public override string ToString()
-    {
-        return $"{Byte} {Mnemonic} {Operand} {Bytes} {Description}";
-    }
-}   
