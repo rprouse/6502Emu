d94e2e67d34107af78192d46985fd5ae1692bff4
Rob Prouse <rob@prouse.org>
2025-05-04 20:04:58 -0400

    Update .gitignore and add assembly test file with compilation instructions

--- 6502Emu.Tests/Test.asm
+++ 6502Emu.Tests/Test.asm
@@ -1,4 +1,8 @@
 ; Simple test program for my 6502 emulator
+; Compile with:
+;   cl65 -o Test.prg -l Test.lst Test.asm
+
+.ORG $8000
   LDA #$DE  ; Load immediate value DE into A
   ADC #$2A  ; Add immediate value 2A to A
   STA $00   ; Store A into memory address 00
